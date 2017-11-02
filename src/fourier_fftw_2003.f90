42afe3102e96c3d74b646eab3d4f44f4a03dd84e
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2017-11-02 12:54:01 -0700

    use unalligned plans with fftw; disable plan destruction due to segfaults.

--- src/fourier_fftw_2003.f90
+++ src/fourier_fftw_2003.f90
@@ -11,16 +11,11 @@ MODULE fourier
   integer, parameter :: sp = kind(1.0)
   integer, parameter :: dp = kind(1.0d0)
 
-  integer :: nof, info=0
-  real(C_DOUBLE), pointer :: in(:) => NULL()
-  complex(C_DOUBLE_COMPLEX), pointer :: out(:) => NULL()
-  complex(C_DOUBLE_COMPLEX), allocatable :: fyy_backup(:)
+  integer :: nof = 0, info = 0
+  real(dp) :: nofinv
 
   type(C_PTR) :: plan = C_NULL_PTR, plan_inv = C_NULL_PTR
-  integer ::  ierr
-
-  ! integer               :: fftw_planning_strategy = FFTW_MEASURE
-  integer               :: fftw_planning_strategy = FFTW_ESTIMATE
+  integer :: ierr
 
   public init_fourier, close_fourier, dfft, dfftinv
 
@@ -28,24 +23,21 @@ CONTAINS
 
   SUBROUTINE init_fourier(nof_in)
 
-    integer :: nof_in
-    type(C_PTR) :: pin, pout
+    integer, intent(in) :: nof_in
+    integer :: fftw_planning_strategy
+    real(C_DOUBLE), allocatable :: in(:)
+    complex(C_DOUBLE_COMPLEX), allocatable :: out(:)
 
-    if (nof_in == 0) return
+    if (nof_in == 0 .or. nof_in == nof) return
+
+    if (nof > 0) call close_fourier()
 
     nof = nof_in
+    nofinv = 1 / dble(nof)
 
-    allocate(fyy_backup(nof/2+1), stat=ierr)
-    if (ierr /= 0) stop 'No room for fyy_backup'
-    
-    pin = fftw_alloc_real( int(nof, C_SIZE_T) )
-    call c_f_pointer( pin, in, [nof] )
-
-    pout = fftw_alloc_complex( int(nof/2+1, C_SIZE_T) )
-    call c_f_pointer( pout, out, [nof/2+1] )
-
-    if ( .not. associated(in) .or. .not. associated(out) ) then
-       stop "Unable to allocate working space for Fourier transform"
+    allocate(in(nof), out(nof/2+1), stat=ierr)
+    if (ierr /= 0) then
+       stop 'Unable to allocate working space for Fourier transform'
     end if
 
     ierr = fftw_import_system_wisdom()
@@ -58,15 +50,19 @@ CONTAINS
        end if
     end if
 
-    if (nof.le.65536) fftw_planning_strategy = FFTW_MEASURE
+    if (nof <= 65536) then
+       fftw_planning_strategy = FFTW_MEASURE
+    else
+       fftw_planning_strategy = FFTW_ESTIMATE
+    end if
 
-    plan = fftw_plan_dft_r2c_1d(nof, in, out, ior(fftw_planning_strategy,FFTW_PRESERVE_INPUT))
-    plan_inv = fftw_plan_dft_c2r_1d(nof, out, in, ior(fftw_planning_strategy,FFTW_DESTROY_INPUT))
+    fftw_planning_strategy = ior(fftw_planning_strategy, FFTW_PRESERVE_INPUT)
+    fftw_planning_strategy = ior(fftw_planning_strategy, FFTW_UNALIGNED)
 
-    call fftw_free(pin)
-    call fftw_free(pout)
-    in => NULL()
-    out => NULL()
+    plan = fftw_plan_dft_r2c_1d(nof, in, out, fftw_planning_strategy)
+    plan_inv = fftw_plan_dft_c2r_1d(nof, out, in, fftw_planning_strategy)
+
+    deallocate(in, out)
 
   END SUBROUTINE init_fourier
 
@@ -74,16 +70,20 @@ CONTAINS
 
   SUBROUTINE close_fourier
 
+    ! Destroying the FFTW plans is disabled for now, after
+    ! observing inexplicable segfaults in some environments.
+
     if (c_associated(plan)) then
-       call fftw_destroy_plan( plan )
+       !call fftw_destroy_plan(plan)
        plan = C_NULL_PTR
     end if
+
     if (c_associated(plan_inv)) then
-       call fftw_destroy_plan( plan_inv )
+       !call fftw_destroy_plan(plan_inv)
        plan_inv = C_NULL_PTR
     end if
 
-    if (allocated(fyy_backup)) deallocate( fyy_backup )
+    nof = 0
 
   END SUBROUTINE close_fourier
 
@@ -107,15 +107,9 @@ CONTAINS
     real(C_DOUBLE), intent(out) :: yy(nof)
     complex(C_DOUBLE_COMPLEX) :: fyy(nof/2+1)
 
-    ! The plan destroys the input
-
-    fyy_backup = fyy
-
     call fftw_execute_dft_c2r(plan_inv, fyy, yy)
-    
-    fyy = fyy_backup
 
-    yy = yy/nof
+    yy = yy * nofinv
 
   END SUBROUTINE dfftinv
 
